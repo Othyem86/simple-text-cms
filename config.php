2f64a7447e697e79ec65d93d06a7fbd5d9463d7c
Emil Föger <emil.foger@gmail.com>
2020-03-16 22:33:24 +0100

    configuration file added for ease of use

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+date_default_timezone_set("Europe/Vienna");
+define("DB_SERVER", "localhost");
+define("DB_USERNAME", "root");
+define("DB_PASSWORD", "");
+define("DB_NAME", "tt_cms");
+define("TEMPLATE_PATH", "./templates");
+define("HOMPAGE_POST_NUM", "5");
+define("ADMIN_USERNAME", "emil");
+define("ADMIN_PASSWORD", "xxxaragornxxx");
+require_once("./classes/Post.php");
+?>
